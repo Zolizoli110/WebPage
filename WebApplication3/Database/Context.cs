82106683c6629e2b54a6b72a1e8cda7024af1778
zolizoli110 <zolika.suveges@gmail.com>
2024-03-20 14:02:31 +0100

    Added news to context

--- WebApplication3/Database/Context.cs
+++ WebApplication3/Database/Context.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication3.Models;
 using WebApplication4.Models;
 
@@ -7,14 +8,16 @@ using Microsoft.EntityFrameworkCore;
 
 public class Context : DbContext
 {
-    public virtual DbSet<User> Users { get; set; }
-    public virtual DbSet<Reservation> Reservations { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<News> News { get; set; }
+
     public Context(DbContextOptions<Context> options) : base(options)
     {
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=postgres");
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
     }
 }
